66766b183e9ca1a1ea3d1be9066dbe9b5825266c
matutamiller <matutamiller@gmail.com>
2018-12-10 20:21:07 +0100

    created a layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Matie Miller's Budget Tracker</p>
+        <Layout>
+          <p>Matie Miller's Budget Tracker</p>
+        </Layout>  
       </div>
     );
   }
