2e3fce6ac37fd82eac37fdb6701f19b78625d7fd
matutamiller <matutamiller@gmail.com>
2018-12-14 17:22:47 +0100

    started working on BudgetTracker container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BudgetTracker from './containers/BudgetTracker/BudgetTracker';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Matie Miller's Budget Tracker</p>
+          <BudgetTracker />
         </Layout>  
       </div>
     );
---
+++ src/containers/BudgetTracker/BudgetTracker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BudgetTracker extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Budget</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BudgetTracker;
